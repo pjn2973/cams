cbdb809acd1398075b2d8223b7ad7a0c0b5eed73
pjn2973 <53032447+pjn2973@users.noreply.github.com>
2019-07-31 21:50:19 -0700

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,11 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='youwillneverguess'
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
+
+app.run(debug=True)
